1e2fd06fc5837fdb36b0fab0357a6b6b2c9c411a
yanhe <yanhe.liu96@gmail.com>
2025-08-17 09:50:12 +0800

    修改vite.config.ts中的base配置为./ / 修复版本问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true
+    // rollupOptions: {
+    //   input: ['src/index.html']
+    // }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
